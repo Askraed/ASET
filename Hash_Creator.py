4915e38a1bd9a4c9074faea5d039898f84a78ff2
Ionut-Alexandru Vlaicu <50250700+Askraed@users.noreply.github.com>
2020-11-08 10:49:39 -0500

    Add files via upload

---
+++ Hash_Creator.py
@@ -0,0 +1,19 @@
+import hashlib, bcrypt
+password = input("Input the password you want to hash...\n>")
+print("\nSHA1:\n")
+for i in range(3):
+    setpass = bytes(password, 'utf-8')
+    hash_object = hashlib.sha1(setpass)
+    guess_pw = hash_object.hexdigest()
+    print(guess_pw)
+print("\nMD5:\n")
+for i in range(3):
+    setpass = bytes(password, 'utf-8')
+    hash_object = hashlib.md5(setpass)
+    guess_pw = hash_object.hexdigest()
+    print(guess_pw)
+print("\nBCRYPT:\n")
+for i in range(3):
+    hashed = bcrypt.hashpw(setpass, bcrypt.gensalt(10))
+    print(hashed)
+
